95d68053f6e642a5f0506bd8cba8897a3883d5ca
jepjep-rgb <jefroyalde@gmail.com>
2022-08-12 14:09:43 +0800

    Refactor bubble sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,14 @@
 def bubble_sort(old_array)
-  sorted = 0
-  until sorted == 1 do
+  while true do
     number_swap = 0
     old_array.each_with_index do |num, idx|
       if idx < (old_array.length - 1) && num > old_array[idx + 1]
-        temp = num
-        old_array[idx] = old_array[idx + 1]
-        old_array[idx + 1] = temp
+        old_array[idx], old_array[idx + 1] = old_array[idx + 1], old_array[idx]
         number_swap = 1
       end
     end
     if number_swap == 0
-      sorted = 1
+      return old_array
     end
   end
-  old_array
 end
